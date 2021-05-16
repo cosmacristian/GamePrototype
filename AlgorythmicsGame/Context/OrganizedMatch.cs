8b643a059b1981ad5e8f97c81da7f6044439f388
cosma cristian <cosma_cris99@yahoo.com>
2021-05-16 17:54:18 +0300

    Project Initialized with Entity Framework and Version Control

---
+++ AlgorythmicsGame/Context/OrganizedMatch.cs
@@ -0,0 +1,35 @@
+﻿using AlgorythmicsGame.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlgorythmicsGame.Context
+{
+
+    public class OrganizedMatch
+    {
+        [Required]
+        [Key]
+        public int MatchId { get; set; }
+
+        [Required]
+        [Column("Type", TypeName = "tinyint")]
+        public int Status { get; set; }
+
+        public int PlayersWaiting { get; private set; }
+
+        public OrganizedMatch()
+        {
+            Status = 1;
+            PlayersWaiting = 1;
+        }
+
+        public void joinMatch()
+        {
+            PlayersWaiting += 1;
+        }
+    }
+}
