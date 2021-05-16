59d1afd6d57673b060166b42b77a2f1f895a56a6
cosma cristian <cosma_cris99@yahoo.com>
2021-05-16 17:50:12 +0300

    Project Initialized with Entity Framework and Version Control

---
+++ AlgorythmicsGame/Context/GameDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlgorythmicsGame.Context
+{
+    public class GameDbContext : DbContext
+    {
+        public GameDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<OrganizedMatch> Matches { get; set; }
+    }
+}
